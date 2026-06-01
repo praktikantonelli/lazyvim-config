6807fe67918005e048e75f3a4fee1342d1bf323c
Luca <luca.antonelli@gmx.ch>
2026-06-01 14:14:05 +0200

    feat(link): start writing custom function to insert link with relative path under cursor

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -29,3 +29,38 @@ end)
 map("n", "<Leader>R", function()
 	overseer.run_template()
 end)
+
+local function insert_markdown_link()
+	-- check if snacks.nvim's picker is available
+	local has_snacks, snacks = pcall(require, "snacks")
+	if not has_snacks then
+		vim.notify("snacks.nvim is not installed", vim.log.levels.ERROR)
+		return
+	end
+
+	local cword = vim.fn.expand("<cWORD>")
+	-- remove trailing punctuation, control and whitespace characters
+	cword = cword:gsub("[%p%c%s]+$", "")
+	if cword == "" then
+		vim.notify("no word under cursor", vim.log.levels.WARN)
+		return
+	end
+
+	snacks.picker.files({
+		search = cword, -- use current WORD under cursor as default input
+		actions = {
+			confirm = function(picker, item)
+				picker:close()
+
+				if item and item.file then
+					local selected_path = item.file
+					vim.notify(selected_path, vim.log.levels.INFO)
+				end
+			end,
+		},
+	})
+end
+
+map("n", "<Leader>rl", function()
+	insert_markdown_link()
+end)
